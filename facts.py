35e9dfe5adb6e40b01fa458faa4b020301e737da
22offat36 <jpz3@zbzoom.net>
2018-08-28 08:09:00 -0400

    Initial commit

---
+++ facts.py
@@ -0,0 +1,12 @@
+def fact():
+
+    number=input("Enter a number to get the factorial: ")
+    initialvalue=0
+    factorial=1
+    run=1
+    for run in range(1,number):
+            print(run, number, initialvalue, factorial)
+            factorial=factorial * (number - initialvalue)
+            initialvalue = initialvalue + 1
+            print(run, number, initialvalue, factorial)
+fact()
